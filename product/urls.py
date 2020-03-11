b0e5dd52eacc1e702c9328be023a6cd92bd3e083
parth <parth@demo.com>
2020-03-11 16:08:29 +0530

    [UPDATE] : implement category using mixins and viewset

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    re_path('categories', views.CategoryGetAndPostView.as_view()),
+    re_path('categories', views.CategoryGetAndPostView.as_view({'get': 'list','post' :  'create'})),
     re_path('products/', views.ProductGetAndPostView.as_view()),
+    # re_path('products/', views.ProductGetAndPostView.as_view({'get': 'list'},{'post': 'create'})),
 
 ]
